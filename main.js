9ea8c7ee4e5aa299f10a8ca9d38db14b342379b4
abhay56gupta <abhay56gupta@gmail.com>
2024-03-29 13:04:20 +0000

    created a login page

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ document.querySelector(".sidebar").style.display="block"
 function show(){
     document.querySelector(".sidebar").style.display="none"
 }
+
+window.addEventListener('mousemove', function() {
+    setTimeout(function() {
+      window.location.href = "login.html";
+    }, 5000);
+  });
+  
+
+
